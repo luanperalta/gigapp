2d45ca4e5aea860b5a27eff9d7e248d6bc86fddf
luanperalta <40551747+luanperalta@users.noreply.github.com>
2018-06-29 20:24:55 -0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,35 @@
 class Order < ApplicationRecord
-  belongs_to :carrier
+ 	# Associations
+ 	belongs_to :carrier
   has_many :items, dependent: :destroy
 
+  # validations
   validates :items, presence: true
-  validates :discount, numericality: true
-  validates :deliveryValue, numericality: true
-
-  # validates :items, presence: true
-
+  
+ 
   accepts_nested_attributes_for :items, allow_destroy: true, reject_if: :all_blank 
-  # permite remover os items em orders
+  
+  # Callbacks
+  before_save :total
+  after_find :total
+
+
+  def total
+  	self.amount = 0
+  	if self.deliveryValue.nil?
+  		self.deliveryValue = 0
+  	end
+  	if self.discount.nil?
+  		self.discount = 0
+  	end
+
+  	self.items.each do |item| 
+  		self.amount += item.amount * item.value 
+  	end
+
+  	self.amount += self.deliveryValue - self.discount
+
+
+  end
+
 end
